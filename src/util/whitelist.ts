9a3985cdc308f5af67d3a0b6d08a35992c0cbe1a
Krysset <maxhagman@hotmail.se>
2022-12-08 16:27:56 +0100

    Added local variable

--- src/util/whitelist.ts
+++ src/util/whitelist.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-let currentWhitelist: [String];
+let currentWhitelist: [string];
 
 export const updateWhitelist = () => {
 	currentWhitelist = JSON.parse(fs.readFileSync("bot-whitelist.json", "utf-8"));
