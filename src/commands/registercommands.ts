0087c2005cf47657a3bbc4a8dc995c82f3dfe966
Krysset <maxhagman@hotmail.se>
2023-06-16 02:56:33 +0200

    Added purgelist to registered commands

--- src/commands/registercommands.ts
+++ src/commands/registercommands.ts
@@ -2,11 +2,13 @@ import { App } from '@slack/bolt';
 import registerPurge from './purge';
 import registerSay from './say';
 import registerGet from './get';
+import registerPurgeList from './purgelist';
 
 const registerCommands = (app: App) => {
 	registerSay(app);
-	registerPurge(app);
 	registerGet(app);
+	// registerPurge(app);
+	registerPurgeList(app);
 };
 
 export default registerCommands;
